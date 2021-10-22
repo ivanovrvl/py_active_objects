b1ea52ec18639fd9f27693b65627410c1cf41f5b
Roman Ivanov <ivanovrvl@gmail.com>
2021-10-22 12:29:52 +0300

    refactoring

--- linked_list.py
+++ linked_list.py
@@ -23,69 +23,69 @@ class DualLinkedListItem:
 class DualLinkedList:
 
     def __init__(self):
-        self.FFirst = self.FLast = None
+        self.first = self.last = None
         self.count = 0
 
     def add(self, item: DualLinkedListItem):
         if item.__list__ is not None:
             self.__list__.Remove(self)
-        if self.FFirst is None:
-            self.FFirst = item
-            self.FLast = item
+        if self.first is None:
+            self.first = item
+            self.last = item
             item.__next__ = None
             item.__prev__ = None
         else:
-            self.FLast.__next__ = item
-            item.__prev__ = self.FLast
+            self.last.__next__ = item
+            item.__prev__ = self.last
             item.__next__ = None
-            self.FLast = item
+            self.last = item
         item.__list__ = self
         self.count += 1
 
     def add_first(self, item: DualLinkedListItem):
-        if self.FFirst is None:
-            self.FFirst = item
-            self.FLast = item
+        if self.first is None:
+            self.first = item
+            self.last = item
             item.__next__ = None
             item.__prev__ = None
         else:
-            self.FFirst.__prev__ = item
-            item.__next__ = self.FFirst
+            self.first.__prev__ = item
+            item.__next__ = self.first
             item.__prev__ = None
-            self.FFirst = item
+            self.first = item
         item.__list__ = self
         self.count += 1
 
     def clear(self):
-        p = self.FFirst;
+        p = self.first;
         while p is not None:
             p2 = p
             p = p.__next__
             p2.__prev__ = None
             p2.__next__ = None
             p2.__list__ = None
-        self.FFirst = None
-        self.FLast = None
+        self.first = None
+        self.last = None
         self.count = 0
 
     def reset(self):
         self.count = 0
-        self.FFirst = None
-        self.FLast = None
+        self.first = None
+        self.last = None
 
     def remove(self, item: DualLinkedListItem):
         if item.__list__ is None: return
         if item.__next__ is None:
             if item.__prev__ is None:
-                self.FFirst = None
-                self.FLast = None
+                self.first = None
+                self.last = None
             else:
                 item.__prev__.__next__ = None
-                self.FLast = item.__prev__
+                self.last = item.__prev__
         else:
             if item.__prev__ is None:
-                self.FFirst = item.__next__
-                self.FFirst.__prev__ = None
+                self.first = item.__next__
+                self.first.__prev__ = None
             else:
                 item.__next__.__prev__ = item.__prev__
                 item.__prev__.__next__ = item.__next__
@@ -95,14 +95,14 @@ class DualLinkedList:
         item.__next__ = None
 
     def remove_first(self) -> DualLinkedListItem:
-        Result = self.FFirst
+        Result = self.first
         if Result is None: return None
         if Result.__next__ is None:
-            self.FFirst = None
-            self.FLast = None
+            self.first = None
+            self.last = None
         else:
-            self.FFirst = Result.__next__
-            self.FFirst.__prev__ = None
+            self.first = Result.__next__
+            self.first.__prev__ = None
         self.count-= 1
         Result.__list__ = None
         Result.__prev__ = None
